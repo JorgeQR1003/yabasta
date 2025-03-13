86f17dfc25162924a69467c68f665398d2c2992c
Jorge Quintero <jorgeqr@cetys.edu.mx>
2025-03-12 20:50:37 -0700

    paren ya

---
+++ styles/header.tsx
@@ -0,0 +1,17 @@
+const headerOptions = (title : string) => ({
+    headerTitle: title,
+    headerStyle: { 
+      backgroundColor: "#1d1c1c", 
+      height: 80, 
+    },
+    headerTitleStyle: { 
+      fontSize: 48, 
+      fontWeight: "bold", 
+      color: "white", 
+      fontFamily: "Shlop", 
+      textAlign: "center", 
+    },
+    headerTitleAlign: "center",
+  });
+
+  export default headerOptions;
